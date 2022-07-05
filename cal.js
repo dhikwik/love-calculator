9dd8a45bb760825a8e9b46e8a745943fb811363f
unknown <19eucs030@skcet.ac.in>
2022-07-05 10:14:39 +0530

    calci

---
+++ cal.js
@@ -0,0 +1,9 @@
+function calc(){
+    var a = document.getElementById("first").value;
+    var b = document.getElementById("second").value;
+    var out=Math.floor(Math.random()*100);
+    
+    document.getElementById("output").innerText=out
+  
+
+}
